be8fe02dc964725a68accccfe9c7d50cae5c8c5a
Bharg Sharma <bharg.sharma@rea-group.com>
2019-11-13 22:52:14 +1100

    Make the form fields responsive

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Inline, TextInput, Button } from "../util/construct-kit";
+import { TextInput, Button } from "../util/construct-kit";
 import styled from "styled-components";
 
 // TODO: add form field validity (i.e. can only be letters) where setSubmitted will be called
@@ -29,33 +29,32 @@ const InputForm = ({onSubmit}: {onSubmit: (verb: String, noun: String) => void})
       border-color: #8B78E6;
     }
   `;
-  const TextInputs = styled.div`
-    @media only screen and (max-width: 300px) {
+  const FlexTextInput = styled(TextInput)`
+    width: 50vh; // want to make the boxes wider but still responsible
+    line-height: 30px;
+    @media (max-width: 500px) {
+      width: 100%;
       flex-direction: column;
     }
   `;
   return (
       <Form>
-        <TextInputs>
-          <Inline>
-            <TextInput
-              id="verb"
-              label="Your Verb"
-              placeholder="construct"
-              help="insert a verb e.g. construct"
-              value={verb}
-              onChange={(e: React.FormEvent<HTMLInputElement>) => setVerb(e.currentTarget.value)} // update the verb from the input
-            />
-            <TextInput
-              id="noun"
-              label="Your Noun"
-              placeholder="price details"
-              help="the object or noun e.g. pricing details"
-              value={noun}
-              onChange={(e: React.FormEvent<HTMLInputElement>) => setNoun(e.currentTarget.value)} // update the noun from the input
-            />
-          </Inline>
-        </TextInputs>
+        <FlexTextInput
+          id="verb"
+          label="Your Verb"
+          placeholder="E.g. construct, fetch, create"
+          help="insert a verb e.g. construct"
+          value={verb}
+          onChange={(e: React.FormEvent<HTMLInputElement>) => setVerb(e.currentTarget.value)} // update the verb from the input
+        />
+        <FlexTextInput
+          id="noun"
+          label="Your Noun"
+          placeholder="E.g. blog, email, post"
+          help="the object or noun e.g. pricing details"
+          value={noun}
+          onChange={(e: React.FormEvent<HTMLInputElement>) => setNoun(e.currentTarget.value)} // update the noun from the input
+        />
         <DarkButton type="submit" onClick={(e: Event) => {e.preventDefault(); onSubmit(verb, noun)}}>Submit</DarkButton>
       </Form>
     );
