fb3bf9f1ffbda77f000dea74b6a84abda0242387
Bharg Sharma <bharg.sharma@rea-group.com>
2019-11-14 15:11:35 +1100

    Displays function names!

--- src/components/NameOptions.tsx
+++ src/components/NameOptions.tsx
@@ -1,22 +1,41 @@
 import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 
 type MyProps = {
-    appState: Array<any>,
-    inputNoun: string
-}
-const NameOptions = ({appState, inputNoun}: MyProps) => {
-    if (appState) {
-        const meta = appState[0]["meta"]
-        if (meta) {
-            const synonyms = meta["syns"][0]
-            console.log(synonyms);
-        }
-    }
-    return (
-        <div></div>
-    );
+  appState: Array<any>;
+  inputNoun: string;
+};
+const Container = styled.div``;
+const NameOptions = ({ appState, inputNoun }: MyProps) => {
+  let options: Array<string> = [];
+  const capitaliseFirstLetter = (noun: string) => {
+    const firstChar = noun.charAt(0).toUpperCase();
+    const capitalisedNoun = firstChar + noun.substr(1);
+    return capitalisedNoun;
+  };
 
-}
+  const combineSynonymWithNoun = (
+    listOfSynonyms: Array<string>,
+    noun: string
+  ) => {
+    const capitalisedNoun = capitaliseFirstLetter(noun);
+    const listOfOptions = listOfSynonyms.map(syn => syn + capitalisedNoun);
+    return listOfOptions;
+  };
+  if (appState) {
+    const meta = appState[0]["meta"];
+    if (meta) {
+      const synonyms: Array<string> = meta["syns"][0];
+      options = combineSynonymWithNoun(synonyms, inputNoun);
+    }
+  }
+  return (
+    <Container>
+      {options.map((option, index) => {
+        return <div key={`function-name-${index}`}>{option}</div>;
+      })}
+    </Container>
+  );
+};
 
 export default NameOptions;
