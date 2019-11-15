d56819fcc75fe397c92ddd63bdc6ca7af672114c
Bharg Sharma <bharg.sharma@rea-group.com>
2019-11-15 11:06:36 +1100

    Add scroll to form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useCallback, RefObject } from "react";
 import { ConstructKit } from "./util/construct-kit";
 import InputForm from "./components/InputForm";
 import About from "./components/About";
@@ -7,46 +7,51 @@ import NameOptions from "./components/NameOptions";
 import styled from "styled-components";
 
 const App: React.FC = () => {
-  const BASE_URL = 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json'
-  const API_KEY = '6d527ea3-6da9-4a03-843c-69a3ab911000';
+  const BASE_URL =
+    "https://www.dictionaryapi.com/api/v3/references/thesaurus/json";
+  const API_KEY = "6d527ea3-6da9-4a03-843c-69a3ab911000";
 
-  const [ appState, setAppState ] = useState([{}]);
-  const [ inputNoun, setNoun ] = useState("");
+  const [appState, setAppState] = useState([{}]);
+  const [inputNoun, setNoun] = useState("");
+  const inputFormRef = useRef(null);
+
+  const scrollToRef = (ref: RefObject<any>) => {
+    ref && window.scrollTo(0, ref.current.offsetTop);
+  };
+  const executeScrollToForm = () => scrollToRef(inputFormRef);
 
   const getSynonyms = (verb: string, noun: string) => {
     fetch(`${BASE_URL}/${verb}?key=${API_KEY}`)
-    .then(response => response.json())
-    .then(data => {
-      setAppState(data);
-      setNoun(noun);
-    });
-  }
+      .then(response => response.json())
+      .then(data => {
+        setAppState(data);
+        setNoun(noun);
+      });
+  };
   const Wrapper = styled.section`
-    background-color: #F5BCBA;
+    background-color: #f5bcba;
   `;
   const Section = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    min-height: 20vh;
+    min-height: 100vh;
   `;
   return (
     <ConstructKit>
       <Wrapper>
         <Section>
           <About />
+          <ArrowButton onClick={executeScrollToForm} />
         </Section>
-        <ArrowButton />
-        <Section>
+        <Section ref={inputFormRef}>
           <InputForm onSubmit={getSynonyms} />
-        </Section>
-        <Section>
-          <NameOptions appState={appState} inputNoun={inputNoun}/>
+          <NameOptions appState={appState} inputNoun={inputNoun} />
         </Section>
       </Wrapper>
     </ConstructKit>
   );
-}
+};
 
 export default App;
--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,33 +1,30 @@
 import React, { useState } from "react";
-import { TextInput, Button } from "../util/construct-kit";
+import { TextInput, Button, H1 } from "../util/construct-kit";
 import styled from "styled-components";
 
 // TODO: add form field validity (i.e. can only be letters) where setSubmitted will be called
 
-const InputForm = ({onSubmit}: {onSubmit: (verb: string, noun: string) => void}) => {
+const InputForm = ({
+  onSubmit
+}: {
+  onSubmit: (verb: string, noun: string) => void;
+}) => {
   const [verb, setVerb] = useState("");
   const [noun, setNoun] = useState("");
   // const [submitted, setSubmitted] = useState(false);
   const Form = styled.form`
     display: flex;
     flex-direction: column;
+    align-items: center;
+    justify-content: center;
     & button {
       align-self: center;
       margin: 20px 0;
     }
   `;
-  const DarkButton = styled(Button)`
-    background-color: #2B2B52;
-    border-color: #2B2B52;
-    font-weight: bold;
-    ::before {
-      content: '💥';
-      font-size: 25px;
-    }
-    :hover {
-      background-color: #8B78E6;
-      border-color: #8B78E6;
-    }
+  const FormTitle = styled(H1)`
+    margin: 3vh 0;
+    color: #2b2b52;
   `;
   const FlexTextInput = styled(TextInput)`
     width: 50vh; // want to make the boxes wider but still responsible
@@ -37,26 +34,52 @@ const InputForm = ({onSubmit}: {onSubmit: (verb: string, noun: string) => void})
       flex-direction: column;
     }
   `;
+  const DarkButton = styled(Button)`
+    background-color: #2b2b52;
+    border-color: #2b2b52;
+    font-weight: bold;
+    ::before {
+      content: "💥";
+      font-size: 25px;
+    }
+    :hover {
+      background-color: #8b78e6;
+      border-color: #8b78e6;
+    }
+  `;
   return (
-      <Form>
-        <FlexTextInput
-          id="verb"
-          label="Your Verb"
-          placeholder="E.g. construct, fetch, create"
-          help="insert a verb e.g. construct"
-          value={verb}
-          onChange={(e: React.FormEvent<HTMLInputElement>) => setVerb(e.currentTarget.value)} // update the verb from the input
-        />
-        <FlexTextInput
-          id="noun"
-          label="Your Noun"
-          placeholder="E.g. blog, email, post"
-          help="the object or noun e.g. pricing details"
-          value={noun}
-          onChange={(e: React.FormEvent<HTMLInputElement>) => setNoun(e.currentTarget.value)} // update the noun from the input
-        />
-        <DarkButton type="submit" onClick={(e: Event) => {e.preventDefault(); onSubmit(verb, noun)}}>Submit</DarkButton>
-      </Form>
-    );
-}
+    <Form>
+      <FormTitle>👇 Generate your function name 👇</FormTitle>
+      <FlexTextInput
+        id="verb"
+        label="Your Verb"
+        placeholder="E.g. construct, fetch, create"
+        help="insert a verb e.g. construct"
+        value={verb}
+        onChange={(e: React.FormEvent<HTMLInputElement>) =>
+          setVerb(e.currentTarget.value)
+        }
+      />
+      <FlexTextInput
+        id="noun"
+        label="Your Noun"
+        placeholder="E.g. blog, email, post"
+        help="the object or noun e.g. pricing details"
+        value={noun}
+        onChange={(e: React.FormEvent<HTMLInputElement>) =>
+          setNoun(e.currentTarget.value)
+        }
+      />
+      <DarkButton
+        type="submit"
+        onClick={(e: Event) => {
+          e.preventDefault();
+          onSubmit(verb, noun);
+        }}
+      >
+        Submit
+      </DarkButton>
+    </Form>
+  );
+};
 export default InputForm;
